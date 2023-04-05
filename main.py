4c26e6f48e2bc96afdac349a08318b6b42ad2df3
Svetlaya2005 <94522412+Svetlaya2005@users.noreply.github.com>
2023-04-05 21:35:18 +0300

    Add file main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from calculation.salary import calculate_salary
 from DB.people import get_employees
 
 if __name__ == 'main':
-    a = calculate_salary()
-    b = get_employees()
+    x = calculate_salary()
+    z = get_employees()
 

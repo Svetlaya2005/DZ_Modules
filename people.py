307f1f5de23b48ddf4257cdf863bd7a87e2a71a7
Svetlaya2005 <94522412+Svetlaya2005@users.noreply.github.com>
2023-04-17 21:12:13 +0300

    Delete people.py

--- people.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-with psycopg2.connect(database="db", user="postgres", password="mypassword") as conn:
-    with conn.cursor() as cur:
-
-        def get_employees():
-            pass

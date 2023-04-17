9ba367d6223e3932775daa3e480c90eaa93337df
Svetlaya2005 <94522412+Svetlaya2005@users.noreply.github.com>
2023-04-17 21:12:35 +0300

    Delete salary.py

--- salary.py
+++
@@ -1,7 +0,0 @@
-def calculate_salary():
-    print('Расчет заработной платы')
-    a = int(input('Введите ежедневный оклад: '))
-    d = int(input('Введите количество отработанных дней: '))
-    s = a * d / 100 * 87
-    print(s)
-calculate_salary()
